2dce835159c0d68b8756738db15483d08ecdd714
Gmluqa <gmluqa@protonmail.com>
2025-06-06 21:13:56 +0300

    fuji added in config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,29 @@
-import type { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox-viem";
+import type { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox-viem';
+import '@nomicfoundation/hardhat-verify';
+import '@nomicfoundation/hardhat-ethers';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: '0.8.28',
+  networks: {
+    localhost: {
+      url: 'http://127.0.0.1:8545',
+      chainId: 31337,
+    },
+    fuji: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      accounts: [process.env.PRIVATE_KEY!],
+      chainId: 43113,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      avalancheFujiTestnet: process.env.SNOWTRACE_API_KEY!,
+    },
+  },
 };
 
 export default config;
